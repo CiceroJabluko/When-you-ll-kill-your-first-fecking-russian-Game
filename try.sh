0182076ab5b20fb4d44b1537a9da0dea60cd8703
CiceroJ <135706366+CiceroJabluko@users.noreply.github.com>
2023-06-06 18:50:09 +0300

    Add files via upload

---
+++ try.sh
@@ -0,0 +1,94 @@
+#!/bin/bash 
+
+clear 
+sleep 1
+
+echo " *make sure youre systems voice is sweet Alex* " 
+ 
+sleep 1  
+
+say Hi there, I am I fecking smart butt and I do some wiseacre magic here. 
+say First of all, please tell me your age..
+
+echo "
+
+
+Only fecking numbers are allowed here!!! Be a good boy and enter it properly."  
+
+echo " "
+read age  
+
+say ok, great. Now, are you ready to get to know when you will kill 
+say your first fecking russian? 
+
+sleep 1
+echo " "
+echo "Only fecking "roger that" is allowed!"
+echo " " 
+read answer 
+
+sleep 1 
+
+say Alrighty 
+
+sleep 1
+
+say Let me compute for you, darling. Thats what you want, right? 
+
+sleep 1 
+
+echo "
+
+COMPUTING"
+echo "*****"
+say bom   
+sleep 1 
+echo "-****"
+say bam 
+sleep 1
+echo "--***" 
+say bim
+sleep 1
+echo "---**"
+say bum
+sleep 1
+echo "----*"
+say boom
+sleep 1 
+echo "-----"
+say bang 
+sleep 1
+
+rich=$((( $RANDOM % 15 ) + $age )) 
+
+say A-ha-ha-ha-ha
+
+sleep 1
+say you will kill your first russian when, you will be, $rich 
+
+echo " "
+echo $rich "------ fecking congrats!" 
+
+say Do you also want me to sing a song for you, ha? 
+
+sleep 1 
+
+say Never mind I will sing it in any case! 
+
+echo "
+ "
+echo " *beautiful robot singing* " 
+say boom boom, boom boom boom, b-boom, yeah, b-boom, yeah baby, boom boom boom  
+say boom boom, boom boom boom, b-boom, yeah, b-boom, yeah baby, boom boom boom do you like it? a-wa-wa-boom 
+say I know you do a-wa-wa
+echo "
+
+Just fecking chill, alright?
+
+" 
+say boom boom, boom boom boom, b-boom, yeah, b-boom, yeah baby, boom boom boom 
+say boom boom, boom boom boom, b-boom, a-wa-wa, b-boom, baby, boom boom boom 
+
+sleep 1
+
+say I hope you fecking enjoy it    
